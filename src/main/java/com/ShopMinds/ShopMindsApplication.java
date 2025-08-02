b9850dd781e9725b6eb2b41fa7bdcc441f502fb0
shimhanmohamad <mohamadshimhan@gmail.com>
2025-08-02 11:37:23 +0530

    feat:Completed the file structure

---
+++ src/main/java/com/ShopMinds/ShopMindsApplication.java
@@ -0,0 +1,13 @@
+package com.ShopMinds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMindsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ShopMinds/ShopMindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShopMinds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
