4ce59c2946167cffbed53f1e71db5ac3ed049f55
shimhanmohamad <mohamadshimhan@gmail.com>
2025-08-23 01:28:18 +0530

    feat: Add essential Spring Boot starters and clean up pom.xml; remove commented CORS configuration

--- src/main/java/com/ShopMinds/ShopMindsApplication.java
+++ src/main/java/com/ShopMinds/ShopMindsApplication.java
@@ -2,9 +2,7 @@ package com.ShopMinds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class ShopMindsApplication {
@@ -13,17 +11,4 @@ public class ShopMindsApplication {
 		SpringApplication.run(ShopMindsApplication.class, args);
 	}
 
-	// Add this method to enable CORS globally
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**")  // Allow all endpoints
-//						.allowedOrigins("http://localhost:5173")  // React's default port
-//						.allowedMethods("GET", "POST", "PUT", "DELETE")
-//						.allowCredentials(true);
-//			}
-//		};
-//	}
 }
